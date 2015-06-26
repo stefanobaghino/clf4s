4d66ee8e0879f67e5d50bd662e33f85d1c81c9b3
Emanuele Blanco <emanuele.blanco@gmail.com>
2015-06-26 13:51:27 +0100

    Instead of pattern matching on a string, unapply now retrieves the value from a Map (faster).

--- src/main/scala/Method.scala
+++ src/main/scala/Method.scala
@@ -2,31 +2,31 @@ package clf4s.http
 
 object Method {
 
-  def apply(methodName: String): Option[Method] = methodName match {
-    case m if m == OPTIONS.toString => Some(OPTIONS)
-    case m if m == GET.toString => Some(GET)
-    case m if m == HEAD.toString => Some(HEAD)
-    case m if m == POST.toString => Some(POST)
-    case m if m == PUT.toString => Some(PUT)
-    case m if m == DELETE.toString => Some(DELETE)
-    case m if m == TRACE.toString => Some(TRACE)
-    case m if m == CONNECT.toString => Some(CONNECT)
-    case _ => None
-  }
+  def apply(methodName: String): Option[Method] = names.get(methodName)
 
   def unapply(method: Method): String = method.toString
 
+  private val names = Map(
+    OPTIONS.toString -> OPTIONS,
+    GET.toString -> GET,
+    HEAD.toString -> HEAD,
+    POST.toString -> POST,
+    PUT.toString -> PUT,
+    DELETE.toString -> DELETE,
+    TRACE.toString -> TRACE,
+    CONNECT.toString -> CONNECT
+  )
 }
 
 sealed abstract class Method {
   override val toString: String = this.getClass.getSimpleName.replace('$', ' ').trim
 }
 
-final object OPTIONS extends Method
-final object GET extends Method
-final object HEAD extends Method
-final object POST extends Method
-final object PUT extends Method
-final object DELETE extends Method
-final object TRACE extends Method
-final object CONNECT extends Method
+object OPTIONS extends Method
+object GET extends Method
+object HEAD extends Method
+object POST extends Method
+object PUT extends Method
+object DELETE extends Method
+object TRACE extends Method
+object CONNECT extends Method

40d4f3025e67a7186a48f624132ad9a1eef66012
Stefano Baghino <stefano@baghino.me>
2015-06-28 01:55:01 +0200

    performance tests refactoring

--- src/test/scala/LogEntryBenchmark.scala
+++ src/test/scala/LogEntryBenchmark.scala
@@ -1,40 +1,13 @@
-package clf4s
+package clf4s.performance
 
-import org.scalameter.api._
-import org.scalameter.execution.SeparateJvmsExecutor
+object LogEntryBenchmark extends LogEntryPerformanceTest {
 
-import scala.io.Source
-
-object LogEntryBenchmark extends PerformanceTest {
-
-  lazy val executor = SeparateJvmsExecutor(
-    new Executor.Warmer.Default,
-    Aggregator.min,
-    new Measurer.Default)
-  lazy val reporter = ChartReporter(ChartFactory.NormalHistogram())
-  lazy val persistor = Persistor.None
-
-
-  try {
-    val logEntriesIterator = Source.fromFile("src/test/resources/perf-test-sample.log")
-    val logEntries = Gen.enumeration("logEntry")(logEntriesIterator.getLines().toList: _*)
-    println("Log sample for performance testing loaded, running performance tests... ")
-    performance of "LogEntry" in {
-      measure method "apply" config (
-        exec.benchRuns -> 64,
-        exec.minWarmupRuns -> 1000,
-        exec.maxWarmupRuns -> 10000,
-        exec.warmupCovThreshold -> 1.0
-        ) in {
-        using(logEntries) in { logEntry =>
-          LogEntry(logEntry)
-        }
+  performance of "LogEntry" in {
+    measure method "apply" in {
+      using(new LogEntryGenerator) in {
+        clf4s.LogEntry(_)
       }
     }
-
-  } catch {
-    case e: Throwable =>
-      println(s"Performance testing skipped for the following reason:\n\t${e.getClass.getName}: ${e.getMessage}")
   }
 
 }
